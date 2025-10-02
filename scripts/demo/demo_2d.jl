0a1623e56b4fdcd2d76eb4b4c277fb5dd60c56bf
Lorenzo Shaikewitz <lorenzos@mit.edu>
2025-10-02 10:26:37 -0400

    add dual variable

--- scripts/demo/demo_2d.jl
+++ scripts/demo/demo_2d.jl
@@ -35,12 +35,21 @@ As = [A1, A2, A3]
 # A5 = [-0.7 -0.5 0; -0.5 -0.072 0.12; 0 0.12 -0.2]
 # As = [A1, A2, A3, A4, A5]
 
+# center = [-0; -0]
+# # A1 = [0 0.5 0; 0.5 1 0; 0 0 3] # x² + y² + x ≤ 0
+# A1 = [-1 0 0; 0 1 0; 0 0 1]
+# B1 = [0 0.5 0; 0.5 5 0; 0 0 5] # x² + y² + x ≤ 0
+# As = [A1]
+Bs = []
+
 plts = []
+Ms = []
+Xs = []
 Hs = []
 vols = []
 for order = 1:4
     # solve
-    H, status, M, X = bound_2d(center, As; order=order, silent=true)
+    H, status, M, X = bound_2d(center, As, Bs; order=order, silent=true)
     println("Status: $status")
     printstyled("Eigvals\n",underline=true)
     display(["Dual"  "Dual Dual"; eigvals(M)[1:3]  eigvals(X)[end-2:end]])
@@ -54,13 +63,23 @@ for order = 1:4
     for A in As
         Plots.contour!(xs, ys, (x,y)->[1;x;y]'*A*[1;x;y], levels=[0], colorbar=false)
     end
+    for B in Bs
+        Plots.contour!(xs, ys, (x,y)->[1;x;y]'*B*[1;x;y], levels=[0], colorbar=false)
+    end
     p=Plots.plot!(xlims=[-1.5,0.5], ylims=(-1,1), aspect_ratio=:equal)
     # p = Plots.plot!(lims=[-3,3], aspect_ratio=:equal)
     push!(plts, p)
 
     push!(Hs, H)
     push!(vols, 1/logdet(H))
+    push!(Ms, M)
+    push!(Xs, X)
 end
+
+# eigvals
+ev_ratio = [eigvals(X)[end-1] / eigvals(X)[end] for X in Xs]
+p_evs = Plots.plot(ev_ratio, xlabel="order", ylabel="λ₂/λ₁")
+
 
 Plots.plot(plts...)
 # TODO: plot all on one plot...
--- src/example_2d.jl
+++ src/example_2d.jl
@@ -2,8 +2,8 @@
 #
 # Lorenzo Shaikewitz, 9/29/2025
 
-function bound_2d(center, As; order=1, silent=false)
-    (order == 1) || return bound_2d_higherorder(center, As; order=order, silent=silent) #error("Not implemented yet!")
+function bound_2d(center, As, Bs=[]; order=1, silent=false)
+    (order == 1) || return bound_2d_higherorder(center, As, Bs; order=order, silent=silent) #error("Not implemented yet!")
 
     # JuMP model
     model = Model(Mosek.Optimizer)
@@ -12,7 +12,9 @@ function bound_2d(center, As; order=1, silent=false)
     end
     @variable(model, log_det_H)
     @variable(model, λ[1:length(As)] .>= 0)
-    # @variable(model, λ >= 0)
+    if length(Bs) > 0
+        @variable(model, η[1:length(Bs)])
+    end
     @variable(model, H[1:2,1:2] ∈ PSDCone())
 
     # objective
@@ -21,9 +23,11 @@ function bound_2d(center, As; order=1, silent=false)
 
     # build M
     M = -[center'*H*center-1  (-H*center)'; -H*center  H]
-    # M += λ*As
     for (i,A) in enumerate(As)
         M += λ[i]*A
+    end
+    for (i,B) in enumerate(Bs)
+        M += η[i]*B
     end
     @constraint(model, psdcon, M >= 0, PSDCone())
 
@@ -37,7 +41,7 @@ function bound_2d(center, As; order=1, silent=false)
     return value.(H), termination_status(model), value.(M), dual(model[:psdcon])
 end
 
-function bound_2d_higherorder(center, As; order=2, silent=false)
+function bound_2d_higherorder(center, As, Bs; order=2, silent=false)
     @polyvar vars[1:2]
 
     # use proxy objective
@@ -53,6 +57,10 @@ function bound_2d_higherorder(center, As; order=2, silent=false)
     # As
     for A in As
         push!(ineq, -[1;vars]'*A*[1;vars])
+    end
+    # Bs
+    for B in Bs
+        push!(eq, [1;vars]'*B*[1;vars])
     end
 
     # use TSSOS to generate redundant constraints
@@ -118,4 +126,39 @@ function bound_2d_higherorder(center, As; order=2, silent=false)
     # display(["Dual"  "Dual Dual"; eigvals(value.(M))  eigvals(X)])
 
     return value.(H), termination_status(model), value.(M), X
+end
+
+
+function bound_2d_dual(center, As, Bs=[]; order=1, silent=false)
+    (order == 1) || return bound_2d_higherorder(center, As, Bs; order=order, silent=silent) #error("Not implemented yet!")
+
+    # JuMP model
+    model = Model(Mosek.Optimizer)
+    if silent
+        set_silent(model)
+    end
+    @variable(model, log_det_S)
+    @variable(model, S[1:2,1:2] ∈ PSDCone())
+    @variable(model, X[1:3,1:3] ∈ PSDCone())
+
+    # objective
+    @objective(model, Min, X[1,1] - log_det_S - 2)
+    @constraint(model, [log_det_S; 1; triangle_vec(S)] ∈ MOI.LogDetConeTriangle(2))
+
+    # S constraint
+    Y = X[2:3,2:3]; y = X[1,2:3]
+    @constraint(model, conS, S == Y - (y*center' + center*y') + X[1,1]*center*center')
+
+    # set constraints
+    for A in As
+        @constraint(model, tr(A'*X) <= 0)
+    end
+    for B in Bs
+        @constraint(model, tr(B'*X) == 0)
+    end
+
+    # solve!
+    optimize!(model)
+
+    return -dual(model[:conS]), termination_status(model), dual(VariableInSetRef(X)), value.(X)
 end
