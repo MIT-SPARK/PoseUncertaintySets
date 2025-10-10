b4defec5c22a433355ffefbc5ad9b589855095a3
Lorenzo Shaikewitz <lorenzos@mit.edu>
2025-10-10 14:54:07 -0400

    add clarification about certificate

--- scripts/demo/demo_2d.jl
+++ scripts/demo/demo_2d.jl
@@ -7,12 +7,12 @@ using LinearAlgebra
 import Plots
 
 # constraints
-center = [-0.2; -0.2]
-A1 = [0 0.5 0; 0.5 1 0; 0 0 1] # x² + y² + x ≤ 0
-A2 = [0 0 0.5; 0 1 0; 0.5 0 0] # x² + y ≤ 0
-A3 = [-0.5 0 -0.5; 0 1 0; -0.5 0 0] # x² - y - 0.5 ≤ 0
-# A4 = [0.05 0 0; 0 -1 0; 0 0 1] # y² - x² + 0.05 ≤ 0
-As = [A1, A2, A3]
+# center = [-0.2; -0.2]
+# A1 = [0 0.5 0; 0.5 1 0; 0 0 1] # x² + y² + x ≤ 0
+# A2 = [0 0 0.5; 0 1 0; 0.5 0 0] # x² + y ≤ 0
+# A3 = [-0.5 0 -0.5; 0 1 0; -0.5 0 0] # x² - y - 0.5 ≤ 0
+# # A4 = [0.05 0 0; 0 -1 0; 0 0 1] # y² - x² + 0.05 ≤ 0
+# As = [A1, A2, A3]
 
 # this is a cool example: you can see it get tighter up to order 3
 # and maybe a good logo?
@@ -23,11 +23,11 @@ As = [A1, A2, A3]
 # As = [A1, A2, A3]
 
 # do AeroAstro logo?
-# center = [-0.65; -0.2]
-# A1 = [0 0.5 0; 0.5 1 0; 0 0 1] # x² + y² + x ≤ 0
-# A2 = [0.2 0 1; 0 -0.5 0; 1 0 0] # -0.5x² + 2y + 0.2 ≤ 0
-# A3 = [0.3 0.5 -0.9; 0.5 0 0; -0.9 0 -5] # -x² - y ≤ 0
-# As = [A1, A2, A3]
+center = [-0.65; -0.2]
+A1 = [0 0.5 0; 0.5 1 0; 0 0 1] # x² + y² + x ≤ 0
+A2 = [0.2 0 1; 0 -0.5 0; 1 0 0] # -0.5x² + 2y + 0.2 ≤ 0
+A3 = [0.3 0.5 -0.9; 0.5 0 0; -0.9 0 -5] # -x² - y ≤ 0
+As = [A1, A2, A3]
 
 # box
 # center = [0; 0]
@@ -48,9 +48,10 @@ As = [A1, A2, A3]
 # As = [A1]
 Bs = []
 
-function certify(H, center, As, Bs)
+function certify(H, center, As)
     # check if any point on the ellipse intersects the set boundary.
     # ellipse: (x - center)'*H*(x - center) = 1
+    # unclear if this is actually a certificate.
     num_pts = 500
     x, y = ellipse_to_surf_2d(H, center, num_pts)
     z = [ones(num_pts)  x  y]
@@ -77,15 +78,15 @@ Ms = []
 Xs = []
 Hs = []
 vols = []
-cert = []
+certs = []
 for order = 1:4
     # solve
     H, status, M, X = bound_2d(center, As, Bs; order=order, silent=true)
     println("Status: $status")
     printstyled("Eigvals\n",underline=true)
     display(["Dual"  "Dual Dual"; eigvals(M)[1:3]  eigvals(X)[end-2:end]])
-    optimal, location = certify(H, center, As, Bs)
-    push!(cert, location)
+    optimal, location = certify_opt(H, center, As)
+    push!(certs, location)
 
     # plot solution
     Plots.plot(ellipse_to_surf_2d(H, center, 100), label=false)
@@ -100,7 +101,7 @@ for order = 1:4
         Plots.contour!(xs, ys, (x,y)->[1;x;y]'*B*[1;x;y], levels=[0], colorbar=false)
     end
 
-    if !isnothing(cert[end])
+    if !isnothing(certs[end])
         Plots.scatter!([location[1]], [location[2]], label=false, ma=0.5)
     end
     
@@ -114,7 +115,7 @@ for order = 1:4
     push!(Xs, X)
 end
 printstyled("Certificates\n",underline=true)
-display(["Vols"  "cert"; vols cert])
+display(["vols"  "certs"; vols certs])
 
 # eigvals
 ev_ratio = [eigvals(X)[end-1] / eigvals(X)[end] for X in Xs]
